56ef8835a91fe1863dc72645e9782503b602ddfd
Stefano Charissis <stefano.charissis@gmail.com>
2021-01-28 22:50:16 +1100

    New: per-server configuration.

--- Watchlist.lua
+++ Watchlist.lua
@@ -1,19 +1,9 @@
 -- Global saved variable: WL
 
-
-local LOCKED_N_LOADED = false;
+local DENUG = true;
 local AH_SHOWING = false;
-local timeGranularity = 86400/24; -- 1 day / 24 = 1hr
+local TIME_WINDOW = 86400/24; -- 1 day / 24 = 1hr
 
-
-function Message(message)
-	if (message) then
-		print(
-		LIGHTBLUE_FONT_COLOR:WrapTextInColorCode("WL: ")
-		.. message
-		)
-	end
-end
 
 function makeSummaryItem(price)
 	price = tonumber(price);
@@ -23,21 +13,24 @@ end
 function addSummary(itemID, price)
 	itemID = tonumber(itemID);
 	price = tonumber(price);
+
 	-- Add summary
-	if WL.db.summary == nil then
-		WL.db.summary = {}
+	local db = getDB();
+	local summary = db.summary;
+	if summary == nil then
+		summary = {}
 	end
-	if WL.db.summary[itemID] == nil then
-        WL.db.summary[itemID] = makeSummaryItem(price)
+	if summary[itemID] == nil then
+        summary[itemID] = makeSummaryItem(price)
 	else
-		local min, max = WL.db.summary[itemID].minPrice, WL.db.summary[itemID].maxPrice
+		local min, max = summary[itemID].minPrice, summary[itemID].maxPrice
 		local tmp = makeSummaryItem(price)
 		tmp.minPrice = math.min(min, price)
 		tmp.maxPrice = math.max(max, price)
 		tmp.lastPrice = price
 		tmp.lastChecked = time()
 		--print(string.format("addSummary(%s,%s): {%s, %s, %s, %s}\n", itemID, price, tmp.minPrice, tmp.maxPrice, tmp.lastPrice, tmp.lastChecked));
-		WL.db.summary[itemID] = tmp
+		summary[itemID] = tmp
 	end
 end
 
@@ -53,7 +46,8 @@ function AddToWatchlist(itemID)
 		Message("Error: could not find item with ID '"..itemID.."'.")
 		return
 	end
-	WL.db.watchlist[itemID] = name;
+	local db = getDB();
+	db.watchlist[itemID] = name;
 	Message("Added "..itemStr(itemID, name).." to the watchlist.");
 end
 
@@ -64,12 +58,13 @@ function AddItemHistory(itemID, price)
 	addSummary(itemID, price);
 
 	-- Add history
-    if WL.db.history[itemID] == nil then
-        WL.db.history[itemID] = {makeHistoryItem(price)}
+	local db = getDB();
+    if db.history[itemID] == nil then
+        db.history[itemID] = {makeHistoryItem(price)}
     else
-        timeSince = time() - WL.db.history[itemID][#WL.db.history[itemID]].ts
-        if timeSince > timeGranularity then
-            table.insert(WL.db.history[itemID], makeHistoryItem(price))
+        timeSince = time() - db.history[itemID][#db.history[itemID]].ts
+        if timeSince > TIME_WINDOW then
+            table.insert(db.history[itemID], makeHistoryItem(price))
         end
 	end
 	--print("DB.history size: "..table.size(WL.db.history))
@@ -77,15 +72,16 @@ function AddItemHistory(itemID, price)
 end
 
 function ScanAH()
+	local db = getDB();
 	local numScanned = 0;
-	for itemID, _ in pairs(WL.db.watchlist) do
+	for itemID, _ in pairs(db.watchlist) do
 		local itemKey = C_AuctionHouse.MakeItemKey(itemID);
 		--itemKey.itemLevel = 190;
 		--print(string.format("item key: (id: %s, ilvl: %s, suff: %s)", itemKey.itemID, itemKey.itemLevel, itemKey.itemSuffix));
 		C_AuctionHouse.SendSearchQuery(itemKey, {}, false);
 		numScanned = numScanned + 1;
 	end
-	Message("Scanned AH for "..table.size(WL.db.watchlist).." watchlisted items.");
+	Message("Scanned AH for "..table.size(db.watchlist).." watchlisted items.");
 end
 
 local f = CreateFrame("Frame", "WatchlistFrame", AuctionHouseFrame);
@@ -99,11 +95,13 @@ function f:OnEvent(event, arg)
 	if event == "ADDON_LOADED" and arg == "Watchlist" then
 		if not WL then
 			-- First ever addon load; init data structures.
-			initWL();
-		end
-		if WL then
-			LOCKED_N_LOADED = true
-			Message("Watchlist loaded successfully!");
+			init();
+		else
+			if not WL[getServer()] then
+				--- First load of addon on this server
+				initDB();
+				Message('Login on a new server detected! Created a new database.');
+			end
 		end
 	elseif event == "PLAYER_LOGOUT" then
 	elseif event == "ITEM_SEARCH_RESULTS_UPDATED" and WL ~= nil then
@@ -141,10 +139,10 @@ end
 
 SLASH_WATCHLIST1 = '/wl';
 function SlashCmdList.WATCHLIST(msg, editbox)
-	if not LOCKED_N_LOADED then 
-		--print("NOT LOCKED AND LOADED!!");
-		return
-	end
+	-- if not LOCKED_N_LOADED then 
+	-- 	--print("NOT LOCKED AND LOADED!!");
+	-- 	return
+	-- end
 	local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)"); -- %d ?
 	--print("cmd: "..cmd..", args: "..(args or ""));
 
@@ -160,7 +158,9 @@ function SlashCmdList.WATCHLIST(msg, editbox)
 		end
 	elseif cmd == "ilvl" then
 		getItemLevel(args)
-	elseif cmd == "reset" or cmd == "clear" then
+	elseif cmd == "clear" then
+		initDB(getServer);
+	elseif cmd == "reset" then
 		initWL();
 	else
 		Message("Unknown command.");
@@ -172,24 +172,54 @@ end
 -- UTILS
 --
 
+function init()
+	initWL();
+	initDB();
+	Message("Watchlist loaded successfully!");
+end
+
 function initWL()
-	WL = {
-		db = {
-			watchlist = {}, -- map[id]name
-			summary = {},
-			history = {
-				-- id -> [{price, ts}]
-			}
+	local server = getServer();
+	WL = {};
+	WL[server] = {};
+	initDB();
+end
+
+function initDB(server)
+	if not server then
+		server = getServer()
+	end
+	if not WL[server] then
+		WL[server] = {};
+	end
+	WL[server]['db'] = {
+		watchlist = {}, -- map[id]name
+		summary = {},
+		history = {
+			-- id -> [{price, ts}]
 		}
 	};
 end
+
+function getServer()
+	local server = GetRealmName();
+	return server;
+end
+
+function getDB()
+	local server = getServer();
+	local db = WL[server]['db'];
+	return db;
+end
+
 function printWatchlist()
-	if table.size(WL.db.watchlist) == 0 then
+	local db = getDB();
+	if table.size(db.watchlist) == 0 then
 		Message("Watchlist is empty.");
 		return
 	end
-	Message("WATCHLIST ("..table.size(WL.db.watchlist).."):");
-	for itemID, name in pairs(WL.db.watchlist) do
+	Message("WATCHLIST ("..table.size(db.watchlist).."):");
+	for itemID, name in pairs(db.watchlist) do
 		Message(" - "..itemStr(itemID, name).." = "..priceStr(itemID));
 	end
 end
@@ -203,106 +233,24 @@ function itemStr(itemID, name)
 end
 
 function priceStr(itemID)
+	local db = getDB();
 	local p = "?g"
-	if WL.db.summary[itemID] then
-		local min = WL.db.summary[itemID].minPrice;
-		local max = WL.db.summary[itemID].maxPrice;
-		local last = WL.db.summary[itemID].lastPrice;
-		local diff = last/max.."% of max";
+	if db.summary[itemID] then
+		local min = db.summary[itemID].minPrice;
+		local max = db.summary[itemID].maxPrice;
+		local last = db.summary[itemID].lastPrice;
+		local diff = (last/max)*100.0 .. "% of max";
 		p = string.format("{min: %s, max: %s, last: %s (%s)}", asGoldStr(min), asGoldStr(max), asGoldStr(last), diff);
 	end
 	return p
 end
 
---
--- UI
---
-
--- function setupWindow()
--- 	local btnScan = CreateFrame("Button", nil, f, "UIPanelButtonTemplate");
--- 	btnScan:SetPoint("CENTER");
--- 	btnScan:SetSize(120, 40);
--- 	btnScan:SetText("Scan AH");
--- 	btnScan:SetScript("OnClick", function(self, button)
--- 		ScanAH();
--- 	end)
--- 	local btnShow = CreateFrame("Button", nil, f, "UIPanelButtonTemplate");
--- 	btnShow:SetPoint("TOP")
--- 	btnShow:SetSize(120, 40)
--- 	btnShow:SetText("Show DB")
--- 	btnShow:SetScript("OnClick", function(self, button)
--- 		printTable(WL.db);
--- 	end)
--- end
--- setupWindow();
-
-local TAB_NAME_HISTORY = "AuctionFrameTabWatchlistHistory"
-
-function getNumTabs()
-	local tabs = 1;
-	while ( _G["AuctionFrameTab"..tabs] ) do
-
-		if ( _G["AuctionFrameTab"..tabs]:GetName() == TAB_NAME_HISTORY ) then
-			return
-		end
-		tabs = tabs + 1;
-	end
-	return tabs
-end
-
-function setupTabs()
-	local numTabs = getNumTabs()
-	local button = CreateFrame("Button", TAB_NAME_HISTORY, AuctionFrame);
-	button:SetText(MARKETWATCHER_HISTORY);
-	--PanelTemplates_TabResize(button, 0);
-	PanelTemplates_DeselectTab(button);
-
-	_G["AuctionFrameTab"..numTabs] = TAB_NAME_HISTORY;
-
-	button = _G["AuctionFrameTab"..numTabs];
-	button:SetParent("AuctionFrame");
-	button:SetPoint("TOPLEFT", _G["AuctionFrameTab"..(numTabs - 1)]:GetName(), "TOPRIGHT", -8, 0);
-	button:SetID(numTabs);
-	button:Show();
-
-	PanelTemplates_SetNumTabs(AuctionFrame, numTabs);
-
-	Watchlist_Frame:SetParent(AuctionFrame);
-	Watchlist_Frame:SetPoint("TOPLEFT", AuctionFrame, "TOPLEFT", 0, 0);
-
-	hooksecurefunc("AuctionFrameTab_OnClick", history.Tab_OnClick);
-	button:SetScript("OnClick", AuctionFrameTab_OnClick);
-end
-
-function tabOnClick(self)
-	if ( self ) then
-		index = self:GetID();
-	else
-		Watchlist_Frame:Hide();
-		return
-	end
-
-	local tab = _G["AuctionFrameTab"..index];
-	if ( tab and tab:GetName() == TAB_NAME_HISTORY ) then
-		PanelTemplates_SetTab(AuctionFrame, index);
-		MarketWatcherScanFrame:Hide();
-
-		-- AuctionFrameTopLeft:SetTexture("Interface\\AuctionFrame\\UI-AuctionFrame-Browse-TopLeft");
-		-- AuctionFrameTop:SetTexture("Interface\\AuctionFrame\\UI-AuctionFrame-Browse-Top");
-		-- AuctionFrameTopRight:SetTexture("Interface\\AuctionFrame\\UI-AuctionFrame-Browse-TopRight");
-		-- AuctionFrameBotLeft:SetTexture("Interface\\AuctionFrame\\UI-AuctionFrame-Browse-BotLeft");
-		-- AuctionFrameBot:SetTexture("Interface\\AuctionFrame\\UI-AuctionFrame-Auction-Bot");
-		-- AuctionFrameBotRight:SetTexture("Interface\\AuctionFrame\\UI-AuctionFrame-Bid-BotRight");
-
-		Watchlist_Frame:Show();
-	else
-		Watchlist_Frame:Hide();
+function Message(message)
+	if (message) then
+		print(LIGHTBLUE_FONT_COLOR:WrapTextInColorCode("WL: ") .. message)
 	end
 end
 
-
-  
--- util
 function table.size(t)
 	local count = 0;
 	for _,_ in pairs(t) do
@@ -324,13 +272,14 @@ function table.val_to_str ( price )
     end
   end
   
-  function table.key_to_str ( k )
-    if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
-      return k
-    else
-      return "[" .. table.val_to_str( k ) .. "]"
-    end
-  end
+function table.key_to_str ( k )
+	if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+		return k
+	else
+		return "[" .. table.val_to_str( k ) .. "]"
+	end
+end
+
 function table.tostring( tbl )
     local result, done = {}, {}
     for k, price in ipairs( tbl ) do
